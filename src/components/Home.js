09230ac19d2d1029e55f6e460888adc737aaacd6
Nazrul islam <jacksparrow.alex@gmail.com>
2021-04-03 18:11:18 +0600

    home component designed with firebaseui, firebaseauth and localstorage, photos will be impoted here

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase, { firebaseui } from "../firebase/Config"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import Photos from "./Photos"
 
-const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
-};
 
-export default Home;
+
+export default class Home extends Component {
+    state = {
+        isLoggedIn: false
+    }
+    ///listen to the firebase auth state and set to the local state;
+    componentDidMount() {
+        this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+            (user) => this.setState({ isLoggedIn: !user })
+        )
+    }
+    ////Make sure we un-register firebase observersw when the component unmounts;
+    componentWillUnmount() {
+        this.unregisterAuthObserver()
+    }
+    render() {
+        if (!this.state.isLoggedIn) {
+            return (
+                <StyledFirebaseAuth uiConfig={firebaseui} firebaseAuth={firebase.auth()} />
+            )
+        }
+        localStorage.setItem("user_id", firebase.auth().currentUser.uid)
+        return (
+            <Photos />
+        )
+    }
+}
