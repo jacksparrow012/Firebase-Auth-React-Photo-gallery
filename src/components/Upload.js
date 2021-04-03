815971ab26322c5f9b190524e447ca3716c0fe4d
Nazrul islam <jacksparrow.alex@gmail.com>
2021-04-03 21:29:25 +0600

    upload file component created with dropzone

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react'
+import firebase from "../firebase/Config"
+import Dropzone from "react-dropzone"
+import Progress from "./Progress"
+export default class Upload extends Component {
+    state = {
+        progress: 0,
+        user_id: localStorage.getItem("user_id"),
+        isUploading: null
+    }
+    handleUploadFiles(files) {
+        for (let i = 0; i < files.length; i++) {
+            const uploadTask = firebase.storage().ref(`images/${files.item(i).name}`)
+                .put(files.item(i))
+            uploadTask.on("state_changed", (snapshot) => {
+                const isUploading = true;
+                this.setState({ isUploading })
+                const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+                this.setState({ progress })
+            }, (err) => {
+            },
+                () => {
+                    firebase.storage.ref("images")
+                        .child(files.item(i).name)
+                        .getDownloadURL()
+                        .then((url) => {
+                            const isUploading = false;
+                            this.setState({ isUploading })
+                            const images = {
+                                url: url,
+                                added: new Date()
+                            }
+                            firebase.firestore().collection(this.state.user_id).add(images).then(res => {
+                                this.props.history.push("/")
+                            })
+                        })
+                })
+        }
+    }
+    render() {
+        return (
+            <div className="mt-5 text-center form-group">
 
-const Upload = () => {
-    return (
-        <div>
-            upload
-        </div>
-    );
-};
+                {this.state.isUploading ? (
 
-export default Upload;
+                    <Progress percentage={this.state.progress} />
+
+                ) : ""}
+                <Dropzone>
+                    {({ getRootProps, getInputProps }) => (
+                        <section>
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()} onDrop={files => this.handleUpload(files)} onChange={(e) => this.handleUpload(e.target.files)} />
+                                <div className="custom-file">
+                                    <input className="custom-file-input" id="customFile" />
+                                    <label className="custom-file-label" htmlFor="customFile">Drag and drop or choose a files</label>
+                                </div>
+                            </div>
+                        </section>
+                    )}
+                </Dropzone>
+
+            </div>
+
+        )
+    }
+}
